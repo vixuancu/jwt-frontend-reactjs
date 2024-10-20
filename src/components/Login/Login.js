e5fb93760af10faf557f5433b015449d2acfa0fa
vixuancu <vixuancu2004@gmail.com>
2024-10-20 10:39:33 +0700

    #9.2 React Router - Điều Hướng Trang Với React

---
+++ src/components/Login/Login.js
@@ -0,0 +1,5 @@
+const Login = (props) => {
+  return <div className="login-container">login</div>;
+};
+
+export default Login;
---
+++ src/components/Navigation/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.scss";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+const Nav = (props) => {
+  return (
+    <div className="topnav">
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <NavLink to="/news">News</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/about">About</NavLink>
+    </div>
+  );
+};
+
+export default Nav;
