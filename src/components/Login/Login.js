d5647310e2fbd0d8580d9563558109a65e9c749d
vixuancu <vixuancu2004@gmail.com>
2024-11-06 15:25:47 +0700

    #9.3.3 Form Login Responsive - Tạo Form Cho Mobile/Laptop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,19 @@
 import "./Login.scss";
 const Login = (props) => {
   return (
-    <div className="login-container mt-3">
+    <div className="login-container ">
       <div className="container">
-        <div className="row">
-          <div className="content-left col-7 ">
+        <div className="row px-3 p-sm-0 ">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block ">
             <div className="brand">Facebook</div>
             <div className="detail">
               learning everything learning everything learning everything
               learning everything learning everything learning everything
             </div>
           </div>
-          <div className="content-right col-5  d-flex flex-column gap-2 py-3">
+
+          <div className="content-right col-sm-5 col-12  d-flex flex-column gap-2 py-3 ">
+            <div className="brand d-sm-none">Facebook</div>
             <input
               type="text"
               className="form-control"
@@ -23,7 +25,11 @@ const Login = (props) => {
               placeholder="password"
             />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center">forgot your password?</span>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                forgot your password?
+              </a>
+            </span>
             <hr />
             <div className="text-center">
               {" "}
